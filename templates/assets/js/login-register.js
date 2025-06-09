c77a5a0747d79077ab69f65cfa6322d290547058
Prashant V Gaikar <1729prashant@gmail.com>
2025-06-09 23:34:40 +0530

    The social login box is always visible.The 'or' line is always visible below the social box.The login and register forms are toggled by the modal logic (showLoginForm and showRegisterForm), which now show/hide the correct .box containers.The modal footer links (create an account and Already have an account?) also toggle the correct form.

--- templates/assets/js/login-register.js
+++ templates/assets/js/login-register.js
@@ -4,26 +4,20 @@
  * 
  */
 function showRegisterForm(){
-    $('.loginBox').fadeOut('fast',function(){
-        $('.registerBox').fadeIn('fast');
-        $('.login-footer').fadeOut('fast',function(){
-            $('.register-footer').fadeIn('fast');
-        });
-        $('.modal-title').html('Register with');
-    }); 
+    $('.loginBox').hide();
+    $('.registerBox').fadeIn('fast');
+    $('.login-footer').hide();
+    $('.register-footer').fadeIn('fast');
+    $('.modal-title').html('Register with');
     $('.error').removeClass('alert alert-danger').html('');
-       
 }
 function showLoginForm(){
-    $('#loginModal .registerBox').fadeOut('fast',function(){
-        $('.loginBox').fadeIn('fast');
-        $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');    
-        });
-        
-        $('.modal-title').html('Login with');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+    $('.registerBox').hide();
+    $('.loginBox').fadeIn('fast');
+    $('.register-footer').hide();
+    $('.login-footer').fadeIn('fast');
+    $('.modal-title').html('Login with');
+    $('.error').removeClass('alert alert-danger').html('');
 }
 
 function openLoginModal(){
